4a3c0d3583ac6328abe39e10c019698f8fc4c76f
Sabrina Luo <luohaojing@gmail.com>
2016-03-21 15:19:06 +0800

    seperated google custom search as a widget

--- source/js/main.js
+++ source/js/main.js
@@ -3,11 +3,14 @@
  * @returns {boolean}
  */
 function search() {
-  document.getElementById('gsc-i-id1').value = document.getElementById('query').value;
-  document.getElementsByClassName('gsc-search-button')[1].click();
+  var $gsc = document.getElementById('gsc-i-id1');
+  if ($gsc) {
+    $gsc.value = document.getElementById('query').value;
+    document.getElementsByClassName('gsc-search-button')[1].click();
+  }
   return false;
 }
 
-document.getElementsByClassName('clear-search')[0].onclick=function(){
+document.getElementsByClassName('clear-search')[0].onclick = function() {
   document.getElementById('query').value = '';
-}
+};
